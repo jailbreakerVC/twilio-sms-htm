fffd54185c12867ea8b9a1063376fb657dfdc9bd
jailbreakerVC <54243183+jailbreakerVC@users.noreply.github.com>
2022-09-17 22:14:12 +0530

    Update sendingSMS.js

--- sendingSMS.js
+++ sendingSMS.js
@@ -1,14 +1,14 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
-const accountSid = "";
-const authToken = "";
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
   .create({
-     body: 'Outside Air is toxic, might be dangerous to go outside',
-     from: '+',
-     to: ''
+     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     from: '+15017122661',
+     to: '+15558675310'
    })
-  .then(message => console.log(message));
+  .then(message => console.log(message.sid));
